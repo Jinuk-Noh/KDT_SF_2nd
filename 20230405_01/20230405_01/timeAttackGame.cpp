b52f50099090a2a16a1a58a7825504125ad20b1d
Jinuk-Noh <beehonestfly@gmail.com>
2023-04-09 16:24:57 +0900

    타임어택 끝말잇기 반목문 문제 수정

--- 20230405_01/20230405_01/timeAttackGame.cpp
+++ 20230405_01/20230405_01/timeAttackGame.cpp
@@ -22,25 +22,25 @@ int main() {
 
 	while (true) {
 
-		cout << "다음 단어를 입력하세요 : ";
-		cin >> inputWord;
-
-		if (inputWords != "" && inputWord[0] != inputWords[inputWords.size() - 1]) {
-			cout << "잘못된 입력니다." << endl;
-			continue;
-		}
-
-		if (checkDuple(&inputWord, &lstWords)) {
-			cout << "중복된 단어입니다. "<<endl;
-			continue;
-		}
-
 		if ((clock() - startTime) / CLOCKS_PER_SEC >= 30) {
 			cout << "타임 오버!" << endl;
 			cout << "총 입력한 단어 갯수 : " << cnt << endl;
 			break;
 		}
 		else {
+			cout << "다음 단어를 입력하세요 : ";
+			cin >> inputWord;
+
+			if (inputWords != "" && inputWord[0] != inputWords[inputWords.size() - 1]) {
+				cout << "잘못된 입력니다." << endl;
+				continue;
+			}
+
+			if (checkDuple(&inputWord, &lstWords)) {
+				cout << "중복된 단어입니다. "<<endl;
+				continue;
+			}
+
 			cnt++;
 			if (inputWords != "") {
 				inputWords = inputWords+"->" + inputWord;
